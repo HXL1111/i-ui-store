7d16a1dca0fc2159169a146e7e829044aa836b86
HXL1111 <12226268+Hxl2521@user.noreply.gitee.com>
2023-01-24 16:54:53 +0800

    整体css

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,20 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
-import Switch from './components/Switch.vue'
-import Button from './components/Button.vue'
-import Dialog from './components/Dialog.vue'
-import Tabs from './components/Tabs.vue'
+import SwitchDemo from './components/SwitchDemo.vue'
+import ButtonDemo from './components/ButtonDemo.vue'
+import DialogDemo from './components/DialogDemo.vue'
+import TabsDemo from './components/TabsDemo.vue'
 const routes = [
   { path: '/', component: Home },
   {
     path: '/doc',
     component: Doc,
     children: [
-      { path: 'switch', component: Switch },
-      { path: 'button', component: Button },
-      { path: 'dialog', component: Dialog },
-      { path: 'tabs', component: Tabs }
+      { path: 'switch', component: SwitchDemo },
+      { path: 'button', component: ButtonDemo },
+      { path: 'dialog', component: DialogDemo },
+      { path: 'tabs', component: TabsDemo }
     ]
   },
 ] as RouteRecordRaw[]
