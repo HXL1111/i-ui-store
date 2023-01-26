73842fd55451ff4d25603154b913c84599bbd019
HXL1111 <12226268+Hxl2521@user.noreply.gitee.com>
2023-01-26 14:37:24 +0800

    修改 switch css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -19,7 +19,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: calc($h/2);
   position: relative;
   span {
@@ -30,14 +30,25 @@ button {
     width: $h2;
     background: white;
     border-radius: calc($h2/2);
-    transition: left 250ms;
+    transition: all 250ms;
   }
 }
 
 button.checked {
-  background: blue;
+  background: #1890ff;
   span {
     left: calc(100% - $h2 - 2px);
+  }
+}
+button:active {
+  > span {
+    width: calc($h2 + 4px);
+  }
+}
+button.checked:active {
+  > span {
+    width: calc($h2 + 4px);
+    margin-left: -4px;
   }
 }
 </style>
