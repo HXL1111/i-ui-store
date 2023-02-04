0cd77e75341e8b05f57c93b12140d0772e3aca3e
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-04 19:13:37 +0800

    完成 导航切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,24 @@
 <template>
-  <div>
-    <div v-for="(t, index) in title" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaultComponent" ::key="index" :is="c" />
+  <div class="i-tabs">
+    <div class="i-tabs-nav">
+      <div :class="{ selected: t === selected }" v-for="(t, index) in title" :key="index" @click="select(t)">
+        {{ t }}
+      </div>
+    </div>
+    <div class="i-tabs-content">
+      <component v-for="(c, index) in defaultComponent" ::key="index" :is="c" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, content) {
     const defaultComponent = content.slots.default()
     defaultComponent.forEach((tag) => {
@@ -16,9 +27,40 @@ export default {
       }
     })
     const title = defaultComponent.map((tag) => tag.props.title)
-    return { defaultComponent, title }
+    const select = (title) => {
+      content.emit('update:selected', title)
+    }
+    return { defaultComponent, title, select }
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.i-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+.selected {
+  color: $blue;
+}
+</style>
