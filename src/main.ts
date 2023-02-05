4270d768efc58d9fa41d2d6a5587a76901d0278a
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-05 14:34:17 +0800

    删除icon

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router';
-import '@svgstore'
 
 const app = createApp(App)
 app.use(router)
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // @ts-nocheck
-import { svgstore } from './src/vite_plugins/svgstore.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgstore()],
+  plugins: [vue()],
   server: {
     host: '0.0.0.0',
   },
