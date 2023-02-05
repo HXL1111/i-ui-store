2406d66cc759a133f368c87596c2ef30d3af974e
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-05 19:14:39 +0800

    引入 github 样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router';
+import 'github-markdown-css'
 
 const app = createApp(App)
 app.use(router)
