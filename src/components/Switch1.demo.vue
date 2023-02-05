1ee7e5922b82b7b9932b2854a79c280649d9d3de
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-05 21:01:31 +0800

    抽离用户需要拷贝的代码

---
+++ src/components/Switch1.demo.vue
@@ -0,0 +1,12 @@
+<template>
+  <Switch v-model:value="bool" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Switch from '../lib/Switch.vue'
+
+const bool = ref(true)
+</script>
+
+<style lang="scss" scoped></style>
