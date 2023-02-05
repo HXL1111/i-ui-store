e4971df755161a7145218f95a79a4a7c26795ac9
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-05 18:25:39 +0800

    基本完成home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,22 +2,31 @@
   <TopNav />
   <div class="banner">
     <h1>i UI</h1>
+    <h2>一个厉害的 Vue UI 框架</h2>
     <h3>助力设计开发者「更灵活」地搭建出「更美」的产品，让用户「快乐工作」～</h3>
     <p class="actions">
       <a href="https://github.com" class="github">Github</a>
       <router-link to="/doc" class="start">开始</router-link>
     </p>
-    <ul class="features">
-      <li class="vue">
-        <Icon name="vue" />
-      </li>
-      <li class="ts">
-        <Icon name="ts" />
-      </li>
-      <li class="easy">
-        <Icon name="easy" />
-      </li>
-    </ul>
+    <div class="features">
+      <ul>
+        <li>
+          <Icon name="vue" />
+          <h2>基于 Vue 3</h2>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <Icon name="ts" />
+          <h2>基于 TypeScript</h2>
+          <p>源代码采用 TypeScript 书写 (非严格检查)</p>
+        </li>
+        <li>
+          <Icon name="easy" />
+          <h2>代码易读</h2>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -39,10 +48,14 @@ $fff: #fff;
   > h1 {
     font-size: 100px;
     color: $blue;
-    padding-top: 32px;
+    padding-top: 48px;
+    text-align: center;
   }
-  > h3 {
+
+  > h3,
+  h2 {
     padding: 8px 0;
+    text-align: center;
   }
   > .actions {
     padding: 32px 0;
@@ -57,9 +70,8 @@ $fff: #fff;
       $h: 58px;
       height: $h;
       line-height: $h;
-      width: 215px;
       border-radius: 8px;
-      padding: 0 8px;
+      padding: 8px 24px;
       font-size: 24px;
       &:hover {
         cursor: pointer;
@@ -82,11 +94,41 @@ $fff: #fff;
     }
   }
   .features {
-    > .vue {
+    margin: 64px auto;
+    width: 400px;
+    @media (min-width: 800px) {
+      width: 800px;
     }
-    > .ts {
+    @media (min-width: 1200px) {
+      width: 1200px;
     }
-    > .easy {
+    > ul {
+      display: flex;
+      flex-wrap: wrap;
+      > li {
+        width: 400px;
+        margin: 16px 0;
+        display: grid;
+        justify-content: start;
+        align-content: space-between;
+        grid-template-areas:
+          'icon title'
+          'icon text';
+        grid-template-columns: 80px auto;
+        grid-template-rows: 1fr auto;
+        > .icon {
+          grid-area: icon;
+          width: 64px;
+          height: 64px;
+        }
+        > h2 {
+          grid-area: title;
+          font-size: 28px;
+        }
+        > p {
+          grid-area: text;
+        }
+      }
     }
   }
 }
