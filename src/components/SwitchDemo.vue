c02e1f258b64588b6fbbb28b1fe64b14b123cf86
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-06 21:49:24 +0800

    改造Button Demo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
 import Demo from './Demo.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
@@ -17,7 +16,6 @@ const Prism = (window as any).Prism
 
 export default {
   components: {
-    Button,
     Demo,
   },
   setup() {
@@ -26,31 +24,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
