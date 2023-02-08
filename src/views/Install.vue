163e63f51e219f0afccb7a94903dd5eca1140dda
HXL <12226268+Hxl2521@user.noreply.gitee.com>
2023-02-08 19:41:19 +0800

    更改 README

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -2,9 +2,9 @@
   <article class="markdown-body">
     <h1>安装</h1>
     <p>打开终端运行下列命令：</p>
-    <pre><code>npm install i-ui</code></pre>
+    <pre><code>npm install i-ui-2</code></pre>
     <p>或</p>
-    <pre><code>yarn add i-ui</code></pre>
+    <pre><code>yarn add i-ui-2</code></pre>
 
     <p>下一节：<a href="#/doc/get-started">开始使用</a></p>
   </article>
